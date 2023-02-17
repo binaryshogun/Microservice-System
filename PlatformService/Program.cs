f32f20662f6f6016dcd013be9e982a271f2b44ef
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-02-17 22:07:56 +0300

    DbContext added to the services container.

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,11 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Adding DbContext to the container
+// Using InMemoryDatabase in development mode 
+builder.Services.AddDbContext<AppDbContext>(options => {
+    options.UseInMemoryDatabase("InMemoryDb");
+});
 
 var app = builder.Build();
 
