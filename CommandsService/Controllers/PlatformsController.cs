c01341920ac1596eab6d6290ae9a22fc95904c8b
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-02-19 14:05:13 +0300

    Platforms controller added into CommandsService.

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [ApiController]
+    [Route("api/c/[controller]")]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+            
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("Inbound POST # Commands Service");
+
+            return Ok("Inbound test from Platforms Controller");
+        }
+    }
+}
