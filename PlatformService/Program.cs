c9666c37dc79a19b9b80262cd4e8a8619af82e04
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-02-20 13:58:38 +0300

    Using SQL Server database provider in production.

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -5,26 +5,38 @@ using PlatformService.Services.Sync.Http;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Adding DbContext to the container
-// Using InMemoryDatabase in development mode 
-builder.Services.AddDbContext<AppDbContext>(options =>
+if (builder.Environment.IsProduction())
 {
-    options.UseInMemoryDatabase("InMemoryDb");
-});
+    Console.WriteLine("Using SQL Server database");
+
+    // Using SQL Server database in production mode
+    builder.Services.AddDbContext<AppDbContext>(options =>
+    {
+        // Connection string using password from user-secrets 'platformdbPassword' secret
+        options.UseSqlServer($"{builder.Configuration.GetConnectionString("PlatformsConnection")}"
+            + $"Password={builder.Configuration["platformsdbPassword"]}");
+    });
+}
+else
+{
+    Console.WriteLine("Using InMemory database");
+
+    // Using InMemoryDatabase in development mode 
+    builder.Services.AddDbContext<AppDbContext>(options =>
+    {
+        options.UseInMemoryDatabase("InMemoryDb");
+    });
+}
 
 // Adding repository implementation for IPlatformRepository dependency request
 builder.Services.AddScoped<IPlatformRepository, PlatformRepository>();
-
 // Adding sync http client to the container
 builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
-
-
 // Register automapper as a service
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
@@ -38,9 +50,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
-
 app.MapControllers();
 
-SeedData.PopulateData(app);
+SeedData.PopulateData(app, app.Environment.IsProduction());
 
 app.Run();
