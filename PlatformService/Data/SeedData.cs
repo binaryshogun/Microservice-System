7dde2376509446e4e86c4db8a0491a19de5f10a2
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-02-20 13:57:40 +0300

    Working with migrations during data seed.

--- PlatformService/Data/SeedData.cs
+++ PlatformService/Data/SeedData.cs
@@ -1,19 +1,34 @@
+using Microsoft.EntityFrameworkCore;
 using PlatformService.Models;
 
 namespace PlatformService.Data
 {
     public static class SeedData
     {
-        public static void PopulateData(IApplicationBuilder app)
+        public static void PopulateData(IApplicationBuilder app, bool isProduction)
         {
             using (var serviceScope = app.ApplicationServices.CreateScope())
             {
-                AddDefaultEntries(serviceScope.ServiceProvider.GetRequiredService<AppDbContext>());
+                AddDefaultEntries(serviceScope.ServiceProvider.GetRequiredService<AppDbContext>(), isProduction);
             }
         }
 
-        private static void AddDefaultEntries(AppDbContext context)
+        private static void AddDefaultEntries(AppDbContext context, bool isProduction)
         {
+            if (isProduction && context.Database.GetPendingMigrations().Any())
+            {
+                Console.WriteLine("Attempting to apply migrations...");
+                try
+                {
+                    context.Database.Migrate();
+                    Console.WriteLine($"Migrations applied successfully!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Failed to apply migrations: {ex.Message}");
+                }
+            }
+
             if (!context.Platforms.Any())
             {
                 Console.WriteLine("Seeding data...");
