645f6158617f89796514c64f0530c43a19ec9d25
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-02-21 13:13:31 +0300

    DTO for published platform added.

---
+++ PlatformService/DTOs/PlatformPublishedDTO.cs
@@ -0,0 +1,11 @@
+namespace PlatformService.DTOs
+{
+    public class PlatformPublishedDTO
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Event { get; set; }
+    }
+}
