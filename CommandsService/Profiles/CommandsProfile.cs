36709b3c6e0851ef77b186df9bf7ae48c49fc497
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-02-21 20:41:07 +0300

    gRPC platform model mapping profile added.

--- CommandsService/Profiles/CommandsProfile.cs
+++ CommandsService/Profiles/CommandsProfile.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using CommandsService.DTOs;
 using CommandsService.Models;
+using PlatformService;
 
 namespace CommandsService.Profiles
 {
@@ -12,10 +13,13 @@ namespace CommandsService.Profiles
             CreateMap<Platform, PlatformReadDTO>();
             CreateMap<Command, CommandReadDTO>();
             CreateMap<CommandCreateDTO, Command>();
-            CreateMap<PlatformPublishedDTO, Platform>().ForMember(p => p.PlatformId, options =>
-            {
-                options.MapFrom(src => src.Id);
-            }).ForMember(p => p.Id, options => options.Ignore());
+            CreateMap<PlatformPublishedDTO, Platform>()
+                .ForMember(p => p.PlatformId, options => options.MapFrom(src => src.Id))
+                .ForMember(p => p.Id, options => options.Ignore());
+            CreateMap<GrpcPlatformModel, Platform>()
+                .ForMember(p => p.PlatformId, options => options.MapFrom(gpm => gpm.PlatformId))
+                .ForMember(p => p.Name, options => options.MapFrom(gpm => gpm.Name))
+                .ForMember(p => p.Commands, options => options.Ignore());
         }
     }
 }
