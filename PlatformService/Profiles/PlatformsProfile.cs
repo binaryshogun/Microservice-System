50c1f2b0af00a66f4d0507c2f77c34d700616768
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-02-21 20:14:46 +0300

    GrpcPlatformModel automapping added.

--- PlatformService/Profiles/PlatformsProfile.cs
+++ PlatformService/Profiles/PlatformsProfile.cs
@@ -12,6 +12,19 @@ namespace PlatformService.Profiles
             CreateMap<Platform, PlatformReadDTO>();
             CreateMap<PlatformCreateDTO, Platform>();
             CreateMap<PlatformReadDTO, PlatformPublishedDTO>();
+            CreateMap<Platform, GrpcPlatformModel>()
+                .ForMember(gpm => gpm.PlatformId, options =>
+                {
+                    options.MapFrom(p => p.Id);
+                })
+                .ForMember(gpm => gpm.Name, options =>
+                {
+                    options.MapFrom(p => p.Name);
+                })
+                .ForMember(gpm => gpm.Publisher, options =>
+                {
+                    options.MapFrom(p => p.Publisher);
+                });
         }
     }
 }
